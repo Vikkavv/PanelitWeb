98e4e39377cbe513746be38386b64edad382ac27
Vikkavv <96725063+Vikkavv@users.noreply.github.com>
2025-03-18 11:31:10 +0100

    Panelit Logo changed and converted into a component to make possible to change svg color through props, navbar component fully finished, with props for the links, and main page background added. Also an animation for the logo has been added.

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,16 +1,49 @@
-function Navbar() {
+import { useEffect, useState } from "react";
+import PanelitLogoSvg from "./panelItLogoSvgComponent"
+
+function createLinksObjects(names, paths){
+    let linkarray = [];
+    for (const i in names) {
+        linkarray.push({"id": i, "name": names[i],"path": paths[i] });
+    }
+    return linkarray.map((link) => {
+        return(
+            <a key={link.id} href={link.path} className="navlink">{link.name}</a>
+        )
+    })
+}
+
+function Navbar(props) {
+    const names = props.texts.split(", ");
+    const paths = props.paths.split(", ");
+    const hiddnBool = props.hiddnLog === "true" ? true : false;
+    const [links, setLinks] = useState([]);
+    useEffect(() => {
+        setLinks(createLinksObjects(names, paths));
+        if(hiddnBool) document.getElementById("logContainer").classList += " hidden";
+    }, []);
+
     return (
-        <div className="navbar">
-            <img className="logo" src="../../public/svgs/PanelitLogo.svg"/>
-            <div className="navlinks">
-                <a href="" className="navlink"><span className="vCentered">Hola</span></a>
-                <span className="navLinkSeparator">/</span>
-                <a href="" className="navlink"><span className="vCentered">Hola</span></a>
-                <span className="navLinkSeparator">/</span>
-                <a href="" className="navlink"><span className="vCentered">Hola</span></a>
+        <nav className="navbar">
+            <div className="logoContainer">
+                <div className="spinningLogo">
+                    <PanelitLogoSvg color="#f0f0f0" />
+                </div>
+                <h2 className="logoHeader margin-0 text-white line-height200">Panelit</h2>
+                <span className="logoSeparator"/>
             </div>
-            <div></div>
-        </div>
+            <div className="navlinks">
+                <div className="navlinks margin-rigth">
+                    {links}
+                </div>
+                <div></div>
+                <div className="logoContainer" id="logContainer">
+                    <a href="" className="navlink">Log in</a>
+                    <span className="logoSeparator"/>
+                    <a href="" className="navlink">Sing up</a>
+                </div>
+            </div>
+        </nav>
     )
 }
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,11 @@
 import Navbar from "../components/NavbarComponent"
 
-document.title = "Wellcome! | Panelit"
+document.title = "Welcome! | Panelit"
 
 function Main() {
     return (
         <>
-            <Navbar></Navbar>
+            <Navbar texts="Subscription Plans, Explore, who we are?" paths="/plans, /home, /whoWeAre" hiddnLog="false"/>
             <h1>Hola</h1>
             <p>Como estás</p>
         </>
